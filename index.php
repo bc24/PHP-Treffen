2e378508e7263068a026fdc95123de1987f1380d
bc24 <frank@bc24.org>
2016-02-28 09:32:53 +0100

    Statische Inhalte erstellt
    -Startseite
    -Über mich
    -Impressung

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php $template = "frank";
+include "ziele.php";
+/**
+ * Created by PhpStorm.
+ * User: Frank
+ * Date: 27.02.2016
+ * Time: 22:17
+ */
+?>
+    <h1>Startseite</h1>
+
+    <h3>Meine erstes CMS</h3>
---
+++ mich.php
@@ -0,0 +1,15 @@
+<?php $template = "frank";
+include "ziele.php";
+/**
+ * Created by PhpStorm.
+ * User: Frank
+ * Date: 27.02.2016
+ * Time: 22:20
+ */
+?>
+<h1>Über mich</h1><br>
+    <h3>Hey! Mein Name ist Frank, ich bin 33 Jahre alt und komme<br> aus Bremen (Deutschland).
+    <br><br>
+    Aktuell mache ich eine Ausbildung zum<br>Fachinformatiker Systemintegration.
+    <br><br>
+    In meiner Freizeit arbeite ich gerne an Webseiten.<br>Ich habe Erfahrung mit HTML, CSS, JavaScript & PHP.</h3>
