3774eb510dab58af05a3e82b1b94623861f82d3a
bc24 <frank@bc24.org>
2016-03-03 22:33:38 +0100

    Neue Startseite

--- index.php
+++ index.php
@@ -7,9 +7,24 @@
  * Time: 22:17
  */
 
+foreach(glob('php/*.php') as $file){
+    require_once $file;
+}
 
 
+if(isset($_GET["page"])){
+    $page = $_GET["page"];
+} else {
+    $page = "startseite";
+}
+
+$path = "contents/$page.php";
+if(file_exists($path)){
+    $content = file_get_contents($path);
+    include("templates/frank.php");
+} else {
+    echo "Error 404";
+}
 
 
 ?>
-
